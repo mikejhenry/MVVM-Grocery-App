e7255df8c1c03e191ec42fd6c6cdd38be07cc7b6
Michael <johnsonmichael302@gmail.com>
2022-05-08 20:39:51 -0400

    Added the GroceryEntities.kt file to the project and pushed it to the repository. This file creates the database and table using a Room implementation.

---
+++ app/src/main/java/com/example/mvvmgroceryapp/GroceryEntities.kt
@@ -0,0 +1,31 @@
+package com.example.groceryapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//This is a data class that will store data.
+//The GroceryEntitites class will create a table in the database.
+//Inside of the database it will contain three columns.
+//Pay close attention to the syntax here as we can use this syntax for
+//future Room Database implementations
+@Entity(tableName = "grocery_items")
+
+data class GroceryEntities(
+
+    //create an itemName variable to store names of grocery items.
+    @ColumnInfo(name = "itemName")
+    var itemName: String,
+
+    //create itemQuantity variable store store quantity of grocery items.
+    @ColumnInfo(name = "itemQuantity")
+    var itemQuantity: Int,
+
+    //create an itemPrice variable to store price of grocery items.
+    @ColumnInfo(name = "itemPrice")
+    var itemPrice: Int
+) {
+    //Primary key is a unique key for use with different databases or tables.
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
